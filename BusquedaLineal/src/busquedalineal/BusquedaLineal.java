801ff7183e82da3183e6824b6523208972648ba9
FeGaBuele <61156489+FeGaBuele@users.noreply.github.com>
2020-04-11 20:01:51 -0500

    Add files via upload

---
+++ BusquedaLineal/src/busquedalineal/BusquedaLineal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package busquedalineal;
+
+/**
+ *
+ * @author FeGa
+ */
+public class BusquedaLineal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        FrmBusqueda frmbusqueda = new FrmBusqueda();
+        frmbusqueda.setVisible(true);
+    }
+    
+}
